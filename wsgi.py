0a0a7d0ff3421dbadae091f41bc8c2336f8a247c
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2020-01-22 11:10:09 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -117,12 +117,8 @@ def mens_page():
   if request.method == "POST":
     print ("in post ",)
     chkbox_val = request.form.getlist('check')
-    print ("chkbox_val is :", chkbox_val)
+    print ("chkbox_val is :", chkbox_val[0])
     
-    chkbox_val = str(chkbox_val).strip('"')
-    print ("chkbox_val is :", chkbox_val)
-    chkbox_val = str(chkbox_val).strip('[]')
-    print ("chkbox_val is :", chkbox_val)
     curc = mysql.connection.cursor()
     query1 = "SELECT s.ITEM_NUMBER, s.DESCRIPTION,s.LONG_DESCRIPTION, s.SKU_ATTRIBUTE_VALUE1,s.SKU_ATTRIBUTE_VALUE2,p.LIST_PRICE,p.DISCOUNT"
     query2 = " FROM XXIBM_PRODUCT_SKU s INNER JOIN XXIBM_PRODUCT_PRICING p WHERE s.ITEM_NUMBER=p.ITEM_NUMBER"
