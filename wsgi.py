896130fbfbd4f6aacd50971b530ecd0ffc3613ef
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2020-01-22 10:56:00 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -119,9 +119,9 @@ def mens_page():
     chkbox_val = request.form.getlist('check')
     print ("chkbox_val is :", chkbox_val[0],chkbox_val[1])
     
-    chkbox_val = str(chkbox_val)..strip('"')
+    chkbox_val = str(chkbox_val).strip('"')
     print ("chkbox_val is :", chkbox_val)
-    chkbox_val = str(chkbox_val)..strip('[]')
+    chkbox_val = str(chkbox_val).strip('[]')
     print ("chkbox_val is :", chkbox_val)
     curc = mysql.connection.cursor()
     query1 = "SELECT s.ITEM_NUMBER, s.DESCRIPTION,s.LONG_DESCRIPTION, s.SKU_ATTRIBUTE_VALUE1,s.SKU_ATTRIBUTE_VALUE2,p.LIST_PRICE,p.DISCOUNT"
